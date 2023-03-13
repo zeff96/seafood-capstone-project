63161b6a2869da2676e92a365e360cb8bde7fe63
shubham <shubham.bhoyar077@gmail.com>
2023-03-13 19:42:13 +0530

    Setup default id's

--- src/modules/getApi.js
+++ src/modules/getApi.js
@@ -1,1 +1,8 @@
-const TESTAPPID = "rnj8XMRPiQ7GI8jXwBi5"
+const TESTAPPID = "rnj8XMRPiQ7GI8jXwBi5";
+const commentsUrl = "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/"+TESTAPPID+"/comments/";
+const likesUrl = "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/"+TESTAPPID+"/likes/";
+const reservationsUrl = "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/"+TESTAPPID+"/reservations/";
+const getSeaFoodUrl = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood";
+const getSeaFoodItem = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=";
+
+export {commentsUrl, likesUrl, reservationsUrl, getSeaFoodUrl, getSeaFoodItem};
