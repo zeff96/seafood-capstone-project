95cf94710dd8d46d7aae4ea97ec4097fad5096c4
Enis <enis.mema@gmail.com>
2023-03-15 09:45:01 +0100

    Fix linters

--- src/modules/reserveCounter.test.js
+++ src/modules/reserveCounter.test.js
@@ -1,26 +1,24 @@
 import { reservationsUrl } from './getApi';
 
-global.fetch = () =>
-  Promise.resolve({
-    json: () =>
-      Promise.resolve([
-        {
-          item_id: 2,
-          username: 'Enis',
-          date_start: '2023-09-22',
-          date_end: '2023-10-16',
-        },
-        {
-          item_id: 2,
-          username: 'Shubam',
-          date_start: '2023-10-15',
-          date_end: '2023-10-16',
-        },
-      ]),
-  });
+global.fetch = () => Promise.resolve({
+  json: () => Promise.resolve([
+    {
+      item_id: 2,
+      username: 'Enis',
+      date_start: '2023-09-22',
+      date_end: '2023-10-16',
+    },
+    {
+      item_id: 2,
+      username: 'Shubam',
+      date_start: '2023-10-15',
+      date_end: '2023-10-16',
+    },
+  ]),
+});
 describe('Testing resevations counter', () => {
   test('count reservations', async () => {
-    const reserving = await fetch(reservationsUrl + '?item_id=2');
+    const reserving = await fetch(`${reservationsUrl}?item_id=2`);
     const reserve = await reserving.json();
     const size = reserve.length;
     expect(size).toBe(2);
